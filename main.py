6e11aaa53259c46d06556e8804bf2f2682d34a0d
EduardoPanizzon <113205827+EduardoPanizzon@users.noreply.github.com>
2025-08-28 12:42:38 -0300

    v2 main.py
    Updated main.py to instantiate and show CadastroPerfis alongside MainWindow.

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from PySide6.QtWidgets import QApplication
 import sys
 from db.database import init_db
 from gui.main_window import MainWindow
+from gui.cadastro import CadastroPerfis
 
 if __name__ == "__main__":
     init_db()
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+
+    main = MainWindow()
+    main.show()
+
+    cadastro = CadastroPerfis()
+    cadastro.show()
+
     sys.exit(app.exec())
