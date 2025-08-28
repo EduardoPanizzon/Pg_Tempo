99300e20546e8e2387458a48f78e7a6e9aba606d
EduardoPanizzon <113205827+EduardoPanizzon@users.noreply.github.com>
2025-08-28 12:41:15 -0300

    v2 schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,18 @@
 CREATE TABLE IF NOT EXISTS perfis (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    idade INTEGER,
-    detalhes TEXT
+    motorista TEXT NOT NULL,
+    placa TEXT NOT NULL,
+    tipo_carro TEXT,
+    ajudante TEXT
 );
 
 CREATE TABLE IF NOT EXISTS registros (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     perfil_id INTEGER NOT NULL,
     data DATE NOT NULL,
-    hora_chegada TIME,
-    habitos TEXT,
+    destino TEXT,
+    hora_chegada TEXT,
+    adiantamento TEXT,
+    manifesto TEXT,
     FOREIGN KEY (perfil_id) REFERENCES perfis(id)
 );
